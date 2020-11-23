fc31ecd10736abbda6c2a2546ed261f162e931b3
Avikant Srivastava <asdavikant@gmail.com>
2020-11-23 18:34:13 +0530

    Credentials are now hidden

---
+++ credentials.py
@@ -0,0 +1,7 @@
+import os
+
+API_KEY = os.environ['API_KEY']
+API_SECRET_KEY = os.environ['API_SECRET_KEY']
+
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = os.environ['HACCESS_TOKEN_SECRETOME']
