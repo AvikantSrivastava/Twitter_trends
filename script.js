3c652af5664a0ea28b3f11984f36f154ecf1adce
Avikant Srivastava <asdavikant@gmail.com>
2020-12-07 03:51:27 +0530

    Trend list is working now!

--- script.js
+++ script.js
@@ -2,14 +2,45 @@ const TrendUrl = "https://twitter-trends-daily.herokuapp.com/alltrends";
 const SentimentUrl = "https://twitter-trends-daily.herokuapp.com";
 
 
-fetch(TrendUrl).then(function (TrendData) {
-  // console.log(TrendData);
+fetch(TrendUrl).then(function (response) {
+  return response.json();
+}).then(function (TrendData) {
+  console.log(TrendData);
+
+  let TrendHTML = ``;
+
+  for (let location in TrendData.trends) {
+    if (location !== 'key') {
+      var list = TrendData.trends[location];
+      // console.log(list);
+      TrendHTML += `
+      <div class="collapsible">
+    <input id="collapsible${location}" type="checkbox" name="collapsible">
+    <label for="collapsible${location}">${location}</label>
+    <div class="collapsible-body">
+    <ul>
+    `;
+
+      for (var item in list) {
+        TrendHTML +=
+          `
+          <li>${list[item]}</li>
+          `;
+      }
+
+      TrendHTML += `</ul></div></div>`
+    }
+    document.getElementById("TrendsTable").innerHTML = TrendHTML;
+
+
+  }
+
 });
 
 fetch(SentimentUrl).then(function (response) {
   return response.json();
 }).then(function (SentimentData) {
-  console.log(SentimentData);
+  // console.log(SentimentData);
 
   let SentimentHTMLIndia = SentimentHTMLWorld = `<tr>
                     <th>#</th>
@@ -19,8 +50,6 @@ fetch(SentimentUrl).then(function (response) {
                     </tr>
                     `;
 
-  // let SentimentHTMLWorld = SentimentHTMLIndia;
-  // console.log(SentimentData);
   for (trend of SentimentData.trends.india) {
     SentimentHTMLIndia += `<tr>
     <td>${trend.rank}</td>
@@ -41,56 +70,7 @@ fetch(SentimentUrl).then(function (response) {
 
   document.getElementById("contentIndia").innerHTML = SentimentHTMLIndia;
   document.getElementById("contentWorld").innerHTML = SentimentHTMLWorld;
-
-
-
 });
 
 
 
-
-// let SentimentHTMLIndia = `<tr>
-//                     <th>#</th>
-//                     <th>Trend</th>
-//                     <th>Positive</th>
-//                     <th>Negative</th> 
-//                     </tr>
-//                     `;
-
-// let SentimentHTMLWorld = SentimentHTMLIndia;
-// console.log(SentimentData);
-// for (trend in SentimentData.trends) {
-//   SentimentHTMLIndia += `<tr>
-//     <td>${trend.rank}</td>
-//     <td>${trend.name}</td>
-//     <td>${trend.positive}</td>
-//     <td>${trend.negative}</td>
-//     </tr>`;
-// }
-
-// document.getElementById("TrendsTable").innerHTML = SentimentHTMLIndia;
-
-
-// function show(data) {
-//   let tab =
-//     `<tr>
-//   <th>#</th>
-//   <th>Trend Name</th>
-//   <th>Positive</th>
-//   <th>Negative</th> 
-//   </tr>
-//   `;
-//   var count = 1;
-//   for (let trend of data.trends) {
-//     tab += `<tr>
-//     <td>${count++}</td>
-//     <td>${trend.name}</td>
-//     <td>${trend.positive}</td>
-//     <td>${trend.negative}</td>
-//     </tr>`;
-//   }
-
-//   document.getElementById("TrendsTable").innerHTML = tab;
-
-
-// }
