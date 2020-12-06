fc932a2058e7864c52c78458a95b415dbbe2a2b2
Avikant Srivastava <asdavikant@gmail.com>
2020-12-07 03:15:33 +0530

    india and world are now working !

--- script.js
+++ script.js
@@ -1,35 +1,96 @@
-const api_endpoint = "https://twitter-trends-daily.herokuapp.com/";
+const TrendUrl = "https://twitter-trends-daily.herokuapp.com/alltrends";
+const SentimentUrl = "https://twitter-trends-daily.herokuapp.com";
 
-async function GetAPI(url) {
-  const response = await fetch(url);
-  var data = await response.json();
-  console.log(data);
 
-  show(data);
-}
+fetch(TrendUrl).then(function (TrendData) {
+  // console.log(TrendData);
+});
 
-GetAPI(api_endpoint);
+fetch(SentimentUrl).then(function (response) {
+  return response.json();
+}).then(function (SentimentData) {
+  console.log(SentimentData);
 
-function show(data) {
-  let tab =
-    `<tr>
-  <th>#</th>
-  <th>Trend Name</th>
-  <th>Positive</th>
-  <th>Negative</th> 
-  </tr>
-  `;
-  var count = 1;
-  for (let trend of data.top_trends) {
-    tab += `<tr>
-    <td>${count++}</td>
+  let SentimentHTMLIndia = SentimentHTMLWorld = `<tr>
+                    <th>#</th>
+                    <th>Trend</th>
+                    <th>Positive</th>
+                    <th>Negative</th> 
+                    </tr>
+                    `;
+
+  // let SentimentHTMLWorld = SentimentHTMLIndia;
+  // console.log(SentimentData);
+  for (trend of SentimentData.trends.india) {
+    SentimentHTMLIndia += `<tr>
+    <td>${trend.rank}</td>
     <td>${trend.name}</td>
     <td>${trend.positive}</td>
     <td>${trend.negative}</td>
     </tr>`;
   }
 
-  document.getElementById("TrendsTable").innerHTML = tab;
+  for (trend of SentimentData.trends.world) {
+    SentimentHTMLWorld += `<tr>
+      <td>${trend.rank}</td>
+      <td>${trend.name}</td>
+      <td>${trend.positive}</td>
+      <td>${trend.negative}</td>
+      </tr>`;
+  }
+
+  document.getElementById("contentIndia").innerHTML = SentimentHTMLIndia;
+  document.getElementById("contentWorld").innerHTML = SentimentHTMLWorld;
 
 
-}
+
+});
+
+
+
+
+// let SentimentHTMLIndia = `<tr>
+//                     <th>#</th>
+//                     <th>Trend</th>
+//                     <th>Positive</th>
+//                     <th>Negative</th> 
+//                     </tr>
+//                     `;
+
+// let SentimentHTMLWorld = SentimentHTMLIndia;
+// console.log(SentimentData);
+// for (trend in SentimentData.trends) {
+//   SentimentHTMLIndia += `<tr>
+//     <td>${trend.rank}</td>
+//     <td>${trend.name}</td>
+//     <td>${trend.positive}</td>
+//     <td>${trend.negative}</td>
+//     </tr>`;
+// }
+
+// document.getElementById("TrendsTable").innerHTML = SentimentHTMLIndia;
+
+
+// function show(data) {
+//   let tab =
+//     `<tr>
+//   <th>#</th>
+//   <th>Trend Name</th>
+//   <th>Positive</th>
+//   <th>Negative</th> 
+//   </tr>
+//   `;
+//   var count = 1;
+//   for (let trend of data.trends) {
+//     tab += `<tr>
+//     <td>${count++}</td>
+//     <td>${trend.name}</td>
+//     <td>${trend.positive}</td>
+//     <td>${trend.negative}</td>
+//     </tr>`;
+//   }
+
+//   document.getElementById("TrendsTable").innerHTML = tab;
+
+
+// }
