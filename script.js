c471f86b5349f1aff681feafe4ee56cd87b45313
Avikant Srivastava <asdavikant@gmail.com>
2020-12-18 14:27:07 +0530

    install button added

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 const TrendUrl = "https://twitter-trends-daily.herokuapp.com/alltrends";
 const SentimentUrl = "https://twitter-trends-daily.herokuapp.com";
 
+let deferredPrompt; // Allows to show the install prompt
+const installButton = document.getElementById("install_button");
+
+window.addEventListener("beforeinstallprompt", e => {
+  console.log("beforeinstallprompt fired");
+  // Prevent Chrome 76 and earlier from automatically showing a prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Show the install button
+  installButton.hidden = false;
+  installButton.addEventListener("click", installApp);
+});
+
+function installApp() {
+  // Show the prompt
+  deferredPrompt.prompt();
+  installButton.disabled = true;
+
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then(choiceResult => {
+    if (choiceResult.outcome === "accepted") {
+      console.log("PWA setup accepted");
+      installButton.hidden = true;
+    } else {
+      console.log("PWA setup rejected");
+    }
+    installButton.disabled = false;
+    deferredPrompt = null;
+  });
+}
+
+window.addEventListener("appinstalled", evt => {
+  console.log("appinstalled fired", evt);
+});
 
 fetch(TrendUrl).then(function (response) {
   return response.json();
